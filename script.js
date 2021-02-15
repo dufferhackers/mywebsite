a48e2181e092d32e757173867383a4f11f031aa1
Sonu Chaudhary <44552641+dufferhackers@users.noreply.github.com>
2021-02-15 11:14:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Ethical Hacking", "Penetration Tester", "Security Researcher", "Rad Teamer", "Bug Hunter"],
+        strings: ["Hacker", "Penetration Tester", "Security Researcher", "Rad Teamer", "Bug Hunter"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Ethical Hacking", "Penetration Tester", "Security Researcher", "Rad Teamer", "Bug Hunter"],
+        strings: ["Hacker", "Penetration Tester", "Security Researcher", "Rad Teamer", "Bug Hunter"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
